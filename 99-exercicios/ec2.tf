7e6ed1fa47de12c4701cb9a821bce8ce30e6b701
manoel1845 <manoel_carlos16@hotmail.com>
2021-12-10 14:27:08 +0000

    add instance

--- 99-exercicios/ec2.tf
+++ 99-exercicios/ec2.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "ec2-az-a" {
   subnet_id                   = aws_subnet.subnet-az-a.id
   ami                         = data.aws_ami.ami-ubuntu.id
   instance_type               = var.instance_type
-  key_name                    = aws_key.kp-mineiro-us
+  key_name                    = aws_key_pair.kp-mineiro-us.id
   associate_public_ip_address = true
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   root_block_device {
