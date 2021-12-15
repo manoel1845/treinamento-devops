4c24c7056811d758847bebcbe75267a7cb41a95c
manoel1845 <manoel_carlos16@hotmail.com>
2021-12-15 12:07:20 +0000

    add firewall rule

--- 97-mineiro-k8s/0-k8s/0-terraform/sg-rules.tf
+++ 97-mineiro-k8s/0-k8s/0-terraform/sg-rules.tf
@@ -81,6 +81,15 @@ resource "aws_security_group_rule" "acessos_haproxy_to_workers" {
   source_security_group_id = aws_security_group.acessos_workers.id
   security_group_id = aws_security_group.acessos_haproxy.id
 }
+resource "aws_security_group_rule" "acessos_workers_to_haproxy" {
+  type             = "ingress"
+  description      = "Libera acessos"
+  from_port        = 0
+  to_port          = 0
+  protocol         = "all"
+  source_security_group_id = aws_security_group.acessos_haproxy.id
+  security_group_id = aws_security_group.acessos_workers.id
+}
 resource "aws_security_group_rule" "acessos_haproxy_ssh" {
   type             = "ingress"
   description      = "Libera acessos"
