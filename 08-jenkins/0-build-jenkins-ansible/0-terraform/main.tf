f57834d0fe9466844f357383353c1963d666f5ef
manoel1845 <manoel_carlos16@hotmail.com>
2021-12-16 19:15:02 +0000

    ajustes no jenkins

--- 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
+++ 08-jenkins/0-build-jenkins-ansible/0-terraform/main.tf
@@ -1,24 +1,41 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "sa-east-1"
 }
 
-data "http" "myip" {
-  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+# data "http" "myip" {
+#   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+# }
+
+data "aws_ami" "ami-ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["099720109477"]
 }
 
 resource "aws_instance" "jenkins" {
-  ami           = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.large"
-  key_name      = "treinamento-turma1_itau"
+  ami                         = data.aws_ami.ami-ubuntu.id
+  subnet_id                   = "subnet-0842414f901483088"
+  instance_type               = "t2.large"
+  key_name                    = "kp-mineiro"
+  associate_public_ip_address = true
+  root_block_device {
+    encrypted   = true
+    volume_size = 30
+  }
   tags = {
-    Name = "jenkins"
+    Name = "jenkins-mineiro"
   }
   vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
 }
 
 resource "aws_security_group" "jenkins" {
-  name        = "acessos_jenkins"
+  name        = "acessos_jenkins_mineiro"
   description = "acessos_jenkins inbound traffic"
+  vpc_id      = "vpc-03d1b7e53da104e7c"
 
   ingress = [
     {
@@ -26,7 +43,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -37,7 +54,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 8080
       to_port          = 8080
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -60,7 +77,7 @@ resource "aws_security_group" "jenkins" {
   ]
 
   tags = {
-    Name = "jenkins-lab"
+    Name = "jenkins-lab-mineiro"
   }
 }
 
@@ -72,6 +89,6 @@ output "jenkins" {
     "private: ${aws_instance.jenkins.private_ip}",
     "public: ${aws_instance.jenkins.public_ip}",
     "public_dns: ${aws_instance.jenkins.public_dns}",
-    "ssh -i ~/Desktop/devops/treinamentoItau ubuntu@${aws_instance.jenkins.public_dns}"
+    "ssh -i ~/kp-mineiro.pem ubuntu@${aws_instance.jenkins.public_dns}"
   ]
 }
